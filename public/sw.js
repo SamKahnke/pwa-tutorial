c59d1d24892e0ada0e68373f00a3b08cafb89a65
SamKahnke <samkahnke@gmail.com>
2018-04-18 17:40:54 -0500

    add sw lifecycle events

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+	console.log('Service Worker installing...', event);
+});
+
+self.addEventListener('activate', function(event) {
+	console.log('Service Worker activating...', event);
+	return self.clients.claim();
+});
